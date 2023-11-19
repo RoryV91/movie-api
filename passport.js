e3e6f8f68d2fc074b5c9d251ab3257d7c8fef4a9
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2023-11-18 22:54:13 -0800

    authentication measures
    -install bcrypt
    -create config.js and put secret there
    -added config.js to gitignore
    -changed LocalStrategy

--- passport.js
+++ passport.js
@@ -1,3 +1,4 @@
+const config = require('./config');
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
@@ -7,40 +8,37 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'Username',
-      passwordField: 'Password',
-    },
-    async (username, password, callback) => {
-      console.log(`${username} ${password}`);
-      await Users.findOne({ Username: username })
-      .then((user) => {
-        if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
-        }
-        console.log('finished');
-        return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'Username',
+        passwordField: 'Password',
+      },
+      async (username, password, callback) => {
+        console.log(`${username} ${password}`);
+        try {
+          const user = await Users.findOne({ Username: username });
+          if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {
+              message: 'Incorrect username or password.',
+            });
+          }
+          console.log('finished');
+          return callback(null, user);
+        } catch (error) {
+          console.error(error);
           return callback(error);
         }
-      })
-    }
-  )
-);
+      }
+    )
+  );
+  
 
-
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, async (jwtPayload, callback) => {
+  passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwtSecret,
+  }, async (jwtPayload, callback) => {
   return await Users.findById(jwtPayload._id)
     .then((user) => {
       return callback(null, user);
