550a5fa0d168f3cee7c3f31eb705194e628e0966
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2023-11-22 18:58:06 -0800

    i dont know :(
    im not very good at this

--- passport.js
+++ passport.js
@@ -1,57 +1,59 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const passportJWT = require('passport-jwt');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const passportJWT = require("passport-jwt");
 const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
-const bcrypt = require('bcrypt');
-const Users = require('./models').User;
-const config = require('./config');
+const bcrypt = require("bcrypt");
+const Users = require("./models").User;
+const config = require("./config");
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'Username',
-      passwordField: 'Password',
-    },
-    async (username, password, callback) => {
-      try {
-        const user = await Users.findOne({ Username: username });
-        if (!user) {
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
-        }
+	new LocalStrategy(
+		{
+			usernameField: "Username",
+			passwordField: "Password",
+		},
+		async (username, password, callback) => {
+			try {
+				const user = await Users.findOne({ Username: username });
+				if (!user) {
+					return callback(null, false, {
+						message: "Incorrect username or password.",
+					});
+				}
 
-        const isValidPassword = await bcrypt.compare(password, user.Password);
-        if (!isValidPassword) {
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
-        }
+				console.log("Stored Hashed Password:", user.password);
+				console.log("Incoming Password:", password);
 
-        return callback(null, user);
-      } catch (error) {
-        console.error(error);
-        return callback(error);
-      }
-    }
-  )
+				const isValidPassword = await bcrypt.compare(password, user.password);
+				if (!isValidPassword) {
+					return callback(null, false, {
+						message: "Incorrect username or password.",
+					});
+				}
+
+				return callback(null, user);
+			} catch (error) {
+				console.error(error);
+				return callback(error);
+			}
+		}
+	)
 );
 
 passport.use(
-  new JwtStrategy(
-    {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: config.jwtSecret,
-    },
-    async (jwtPayload, callback) => {
-      try {
-        const user = await Users.findById(jwtPayload.id);
-        return callback(null, user);
-      } catch (error) {
-        return callback(error);
-      }
-    }
-  )
+	new JwtStrategy(
+		{
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			secretOrKey: config.jwtSecret,
+		},
+		async (jwtPayload, callback) => {
+			try {
+				const user = await Users.findById(jwtPayload.id);
+				return callback(null, user);
+			} catch (error) {
+				return callback(error);
+			}
+		}
+	)
 );
-
